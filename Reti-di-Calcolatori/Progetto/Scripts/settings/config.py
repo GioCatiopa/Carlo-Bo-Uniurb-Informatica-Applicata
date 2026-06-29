03adf7bfe13c40c46011568001bcd9b156c4919f
Giovanni Amadori <74784838+GioCatiopa@users.noreply.github.com>
2026-06-29 22:29:21 +0200

    Aggiunto file di configurazione

---
+++ Reti-di-Calcolatori/Progetto/Scripts/settings/config.py
@@ -0,0 +1,10 @@
+# Settings del server
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 9000
+
+# Settings del MitM
+MITM_HOST = "127.0.0.1"
+MITM_PORT = 8000
+
+# Dimensione buffer ricezione dati TCP
+BUFFER_SIZE = 1024
